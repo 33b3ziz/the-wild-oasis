3ff47c19bbf580067500c7eda7fa43939e15870d
33b3ziz <egyptianhack325@gmail.com>
2024-01-31 01:40:21 +0200

    Add breakfast option

--- src/features/check-in-out/useCheckin.ts
+++ src/features/check-in-out/useCheckin.ts
@@ -3,14 +3,26 @@ import { updateBooking } from "../../services/apiBookings";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
+type Checkin = {
+  bookingId: number;
+  breakfast:
+    | {
+        hasBreakfast: boolean;
+        extrasPrice: number;
+        totalPrice: number;
+      }
+    | object;
+};
+
 export function useCheckin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: checkin, isPending: isCheckingIn } = useMutation({
-    mutationFn: (bookingID: number) =>
-      updateBooking(+bookingID, {
+    mutationFn: ({ bookingId, breakfast }: Checkin) =>
+      updateBooking(+bookingId, {
         status: "checked-in",
         isPaid: true,
+        ...breakfast,
       }),
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked in!`);
