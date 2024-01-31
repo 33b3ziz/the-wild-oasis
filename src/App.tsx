717d7cae12bda9db5f3fb831fbe6ba5dc41629e5
33b3ziz <egyptianhack325@gmail.com>
2024-01-31 07:19:42 +0200

    added authorization

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Cabins from "./pages/Cabins";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,7 +31,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
@@ -40,8 +47,8 @@ function App() {
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="*" element={<PageNotFound />} />
+            <Route path="account" element={<Account />} />
           </Route>
-          <Route path="account" element={<Account />} />
           <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
---
+++ src/ui/ProtectedRoute.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPageSpinner = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. If the user is not authenticated, redirect to the login page
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+  // 3. While loading, show a loading indicator
+  if (isLoading)
+    return (
+      <FullPageSpinner>
+        <Spinner />
+      </FullPageSpinner>
+    );
+  // 4. If the user is authenticated, show the children
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
