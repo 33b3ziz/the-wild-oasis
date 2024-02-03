3cf0dbbe3a96864dca0dfea717f05341caea40d6
33b3ziz <egyptianhack325@gmail.com>
2024-02-03 06:00:20 +0200

    small fix

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("yofebo7300@alibrs.com");
+  const [password, setPassword] = useState("111111111");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
